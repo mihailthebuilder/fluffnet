83fcf603f494c86da98aec0ccf56bfbb8211f58a
Mihail Marian <mihailmarian12@gmail.com>
2021-10-15 16:01:30 +0100

    bugged attempt to output model

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from fastai.vision.all import load_learner
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+
+def main():
+    learn = load_learner("./fluffy-model.pkl")
+    
+    fluffy,_,probs = learn.predict("./test-img.jpeg")
+
+    output = f"Is this fluffy?: {fluffy}. Probability it's fluffy: {probs[1].item():.6f}"
+
+    return output
